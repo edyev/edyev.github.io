47de1a6a6d26dc8819270a904bf4179c1e3af0bc
Eddie Vazquez <lalo_psp@hotmail.com>
2016-03-02 10:55:58 -0600

    Update composicion.formas.js

--- rv/composicion.formas.js
+++ rv/composicion.formas.js
@@ -25,7 +25,7 @@ function setup(){
 
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerHeight*0.95, window.innerHeight*0.95);
-  document.body.appendChild(renderer, domElement);
+  document.body.appendChild(renderer.domElement);
 
 }
 
