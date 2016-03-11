ad51eeb8257358ebd5e670672b6e1a85b7eb6bc1
Eddie Vazquez <lalo_psp@hotmail.com>
2016-03-11 10:27:49 -0600

    Create movingRobot.js

---
+++ rv/movingRobot.js
@@ -0,0 +1,69 @@
+function Pierna(){
+  THREE.Object3D.call(this);
+  this.pierna = new THREE.Mesh( new THREE.CylinderGeometry(0.1,0.1,2) );
+  this.pie = new THREE.Mesh( new THREE.TorusGeometry(0.3,0.05,16,100));
+  this.add(this.pierna);
+  this.add(this.pie);
+  
+}
+
+
+ // leg1.position.x = .25,
+  //leg1.position.y = -2.5,
+  //foot1.position.x = 0.15 + 0.05 + Math.sin(.2),
+//  foot1.position.y = -3.55,
+//  foot1.rotation.x = 3.1416/2;
+//  foot2.rotation.x = 3.1416/2;
+  //foot2.position.x = -0.15 -0.05 - Math.sin(.2),
+  //foot2.position.y = -3.55,
+
+ var headForm = new THREE.TorusKnotGeometry(0.5, 0.1, 100, 10 );
+  //var headForm = new THREE.DodecahedronGeometry(0.5);
+  var esferaForma = new THREE.SphereGeometry(1);
+  var legForm = new THREE.CylinderGeometry(0.1,0.1,2);
+  var bodyForm = new THREE.LatheGeometry(points);
+
+Pierna.prototype = new THREE.Object3D();
+function setup(){
+   var points = [];
+  points.push(new THREE.Vector2(0,2));
+  points.push(new THREE.Vector2(0.7,-0.5));
+  points.push(new THREE.Vector2(2,-1.5));
+  points.push(new THREE.Vector2(0,-1.5));
+  cuerpo = new THREE.Mesh( new THREE.LatheGeometry(points));
+  cabeza = new THREE.Mesh(new THREE.TorusKnotGeometry(0.5,.1,100,10));
+  piernaD = new Pierna();
+  piernaI = new Pierna();
+  cabeza.position.y = 3;
+  piernaD.position.z = -0.25;
+  piernaI.position.z = 0.25;
+  step = 0.01;
+  escena = new THREE.Scene();
+  escena.add( cuerpo );
+  escena.add( cabeza );
+  escena.add( piernaD );
+  escena.add( piernaI );
+  camara = new THREE.PerspectiveCamera();
+  camara.position.z = 20;
+  
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerHeight*0.95, window.innerHeight*0.95 );
+  document.body.appendChild(renderer.domElement);
+}
+
+function loop(){
+  requestAnimationFrame( loop );
+  renderer.render(escena, camara);
+  if( Math.abs(piernaD.rotation.z) > 0.5 )
+    step = -step;
+    
+  piernaD.rotation.z += step;
+  piernaI.rotation.z -= step;
+  
+}
+
+var escena, camara, renderer;
+var step, piernaD, piernaI,cabeza,cuerpo;
+
+setup();
+loop();
