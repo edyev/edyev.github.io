208550fa91b7e9f2bc2e1c75810695e40e3dc6cd
Eddie Vazquez <lalo_psp@hotmail.com>
2016-03-11 00:30:29 -0600

    Update robot.js

--- rv/robot.js
+++ rv/robot.js
@@ -1,23 +1,29 @@
 function setup(){
+  var points = [];
+  puntos.push(new THREE.Vector2(0,2));
+  puntos.push(new THREE.Vector2(0.3,-1));
+  puntos.push(new THREE.Vector2(1.5,-2));
+  
   
   var headForm = new THREE.TorusKnotGeometry(0.5, 0.1, 100, 10 );
   var esferaForma = new THREE.SphereGeometry(1);
-  var cilindroForma = new THREE.CylinderGeometry(0.5, 0.5, 4);
-
+  
+  var bodyForm = new THREE.LatheGeometry(points);
   var material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
  
   var head  = new THREE.Mesh(headForm,material);
   var esfera2 = new THREE.Mesh(esferaForma);
-  var cilindro = new THREE.Mesh(cilindroForma);
+  var body = new THREE.Mesh(bodyForm, material);
 
 
   esfera2.position.y = -2;
-  head.position.y = 2;
+  head.position.y = 3;
+
   var forma = new THREE.Geometry();
 
   THREE.GeometryUtils.merge(forma, head);
   THREE.GeometryUtils.merge(forma, esfera2);
-  THREE.GeometryUtils.merge(forma, cilindro);
+  THREE.GeometryUtils.merge(forma, body);
 
   malla = new THREE.Mesh(forma);
   escena = new THREE.Scene();
