3317f0a905859075084edeb594522d5e6b78ffc6
Eddie Vazquez <lalo_psp@hotmail.com>
2016-03-16 10:46:11 -0600

    Update textura1.js

--- rv/textura1.js
+++ rv/textura1.js
@@ -11,7 +11,7 @@ function setup(){
   camara = new THREE.PerspectiveCamera();
   camara.position.z = 5;
   renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth*0.95, window.innerHeight*0.95);
+  renderer.setSize(window.innerHeight*0.95, window.innerHeight*0.95);
   document.body.appendChild(renderer.domElement);
   
 
