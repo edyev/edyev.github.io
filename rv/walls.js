47928ba7fb73a5cb387da84c7c6ea59066733295
Eddie Vazquez <lalo_psp@hotmail.com>
2016-04-25 11:58:10 -0500

    Update walls.js

--- rv/walls.js
+++ rv/walls.js
@@ -1,6 +1,6 @@
 function Wall(){
   THREE.Object3D.call(this);
-  this.wall = new THREE.Mesh(new THREE.BoxGeometry(10,5,0.2),
+  this.wall = new THREE.Mesh(new THREE.BoxGeometry(20,5,0.2),
                        new THREE.MeshBasicMaterial({map: THREE.ImageUtils.loadTexture('./walls.jpg')}));
   this.add(this.wall);
 }
@@ -14,12 +14,12 @@ wall2 = new Wall();
 wall3 = new Wall();
 wall4 = new Wall();
 
-wall1.position.z = -5;
+wall1.position.z = -10;
 wall2.rotation.y = 3.1416 / 2;
-wall2.position.x = 5;
+wall2.position.x = 10;
 wall3.rotation.y = 3.1416 / 2;
-wall3.position.x = -5;
-wall4.position.z = 5;
+wall3.position.x = -10;
+wall4.position.z = 10;
 
 
 var iluminacion = new THREE.PointLight(0xFFFFFF);
@@ -34,7 +34,7 @@ escena.add(iluminacion);
 
 camara = new THREE.PerspectiveCamera();
 camara.position.z = 0;
-camara.position.y = 20;
+camara.position.y = 30;
 camara.rotation.x = -3.1416 / 2 + 0.07;
 renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerHeight*0.95, window.innerHeight*0.95);
