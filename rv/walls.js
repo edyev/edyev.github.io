a500cf77b4d217b934a90d5b5929b3df0ed9feb5
Eddie Vazquez <lalo_psp@hotmail.com>
2016-04-25 11:23:21 -0500

    Update walls.js

--- rv/walls.js
+++ rv/walls.js
@@ -14,7 +14,7 @@ escena.add(iluminacion);
 camara = new THREE.PerspectiveCamera();
 camara.position.z = 15;
 camara.position.y = 5;
-camara.rotation.x = 1.1;
+//camara.rotation.x = 1.1;
 renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerHeight*0.95, window.innerHeight*0.95);
 document.body.appendChild( renderer.domElement);
