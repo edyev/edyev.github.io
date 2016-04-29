d5f7b91857d7c3bd72a7637787645b29eafc0c40
Eddie Vazquez <lalo_psp@hotmail.com>
2016-04-29 13:21:53 -0500

    Update walls.js

--- rv/walls.js
+++ rv/walls.js
@@ -10,11 +10,14 @@ Wall.prototype = new THREE.Object3D();
 function setup(){
 THREE.ImageUtils.crossOrigin = '';
 robot = new THREE.Mesh(new THREE.SphereGeometry(.8,30,30), new THREE.MeshBasicMaterial());
+cyl = new TRREE.Mesh(new THREE.CylinderGeometry(3,1,1), new THREE.MeshBasicMaterial());
+
 wall1 = new Wall();
 wall2 = new Wall();
 wall3 = new Wall();
 wall4 = new Wall();
 
+cyl.position.y = 7;
 wall1.position.z = -10;
 wall2.rotation.y = 3.1416 / 2;
 wall2.position.x = 10;
@@ -30,7 +33,8 @@ escena.add(wall1);
 escena.add(wall2);
 escena.add(wall3);
 escena.add(wall4);
-escena.add(robot);
+escena.add(robot, cyl);
+
 
 
 
@@ -66,6 +70,6 @@ robot.position.z += stepz;
 renderer.render(escena, camara);
 }
 
-var camara, escena, renderer,wall1,wall2,wall3,wall4,raycaster,robot,stepx,stepz;
+var camara, escena, renderer,wall1,wall2,wall3,wall4,raycaster,robot,stepx,stepz,cyl;
 setup();
 loop();
