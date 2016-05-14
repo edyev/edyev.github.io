5d2d9250fe6d5860ced0b3f7e813af54d0e850f1
Eddie Vazquez <lalo_psp@hotmail.com>
2016-05-14 14:40:37 -0500

    Update keycamara.js

--- rv/keycamara.js
+++ rv/keycamara.js
@@ -36,7 +36,7 @@ function setup(){
 
         	}
         	else{
-        		 camara = new THREE.OrthographicCamera( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, 0.1, 1000 );
+        		 camara = new THREE.OrthographicCamera(75,aspect,  0.1, 1000 );
         	
         	}
         	//
