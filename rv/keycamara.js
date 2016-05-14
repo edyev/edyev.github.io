840e27b4c66dd995ece63e0bf69d2f8cd287b201
Eddie Vazquez <lalo_psp@hotmail.com>
2016-05-14 16:39:57 -0500

    Update keycamara.js

--- rv/keycamara.js
+++ rv/keycamara.js
@@ -14,7 +14,7 @@ function setup(){
 	  var material = new THREE.MeshBasicMaterial();
 	  var forma = new THREE.DodecahedronGeometry(10,0);
 	 // var forma = new THREE.BoxGeometry(10,10,10,10);
-	 malla = new THREE.Mesh(forma, material);
+	 malla = new THREE.Mesh(forma, new THREE.LambertMaterial({color:0xca00c}));
 	 
 	  escena.add(malla);
 	  }
