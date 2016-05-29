80ae3cfbbfaad49c150b3dd7e66a04ceeb6bbf5d
Eddie Vazquez <lalo_psp@hotmail.com>
2016-05-28 20:37:19 -0500

    Update agentSetup.js

--- rv/agentSetup.js
+++ rv/agentSetup.js
@@ -77,7 +77,7 @@ Robot.prototype = new Agent();
 Robot.prototype.sense = function(environment){
   this.sensor.set(this.position, new THREE.Vector3(Math.cos(this.rotation.z),Math.sin(this.rotation.z),0));
   var obstaculo= this.sensor.intersectObjects(environment.children,true);
-  if((obstaculo.length>0 && (obstaculo[0].distance<=.5)))
+  if((obstaculo.length>0 && (obstaculo[0].distance<=.1)))
     this.sensor.colision=true;
   else
     this.sensor.colision=false;
