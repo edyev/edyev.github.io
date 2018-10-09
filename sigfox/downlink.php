499fd63d8ee2cf0d08da81d03b51ec3de61a6cd5
Eddie Vazquez <lalo_psp@hotmail.com>
2018-10-09 11:13:49 -0500

    Update downlink.php

--- sigfox/downlink.php
+++ sigfox/downlink.php
@@ -1,13 +1,4 @@
-<?php
-   $_id = $_GET["id"];
-   $_time = $_GET["time"];
-   $_ack = $_GET["ack"];
-   $_data = $_GET["data"];
-   if ( $_ack == "true" ) {
-     echo "{";
-     echo "\"". $_id ."\" : { \"downlinkData\" : \"0102030405060708\" }";
-     echo "}";
-   } 
-   header("HTTP/1.0 200 OK");
-   header("Content-Type : application/json");
-?>
+{
+
+   "413A9C" : { "downlinkData" : "0102030405060708"}
+}
